675f5d5b87029ced32de9520737d64bcc1dcb055
Johannes Drever <drever@lrz.uni-muenchen.de>
2015-11-02 01:39:59 +0100

    Further styling

--- src/main.hs
+++ src/main.hs
@@ -370,10 +370,12 @@ instance B.ToMarkup (Project, [Issue]) where
              BH.script BH.! A.src "../js/jquery-2.1.4.js" $ "" 
              BH.script BH.! A.src "../js/lantis.js" $ ""
              BH.script $ BH.toHtml $ "lantis.projectId = " ++ show (projectId p)
-        BH.body $ BH.img BH.! A.src "../img/lantis.png"
-        BH.h1 $ BH.toHtml (projectName p)
-        controls
-        mapM_ (column is) (projectStatus p)
+        BH.div BH.! A.id "header" $ do
+             BH.body $ BH.img BH.! A.src "../img/lantis.png"
+             BH.h1 $ BH.toHtml (projectName p)
+        BH.div BH.! A.id "content" $ do
+             controls
+             mapM_ (column is) (projectStatus p)
 
 controls :: BH.Markup
 controls = 
@@ -381,14 +383,14 @@ controls =
         BH.button BH.! A.onclick "lantis.createIssue(lantis.projectId)" $ "New issue"
 
 column :: [Issue] -> Status -> BH.Markup
-column is s = BH.div BH.! A.id (BH.toValue $ show s) BH.! A.class_ "column" BH.! A.draggable (BH.toValue True) BH.! A.ondragover "lantis.allowDrag(event)" BH.! A.ondrop "lantis.drop(event)" $ do
+column is s = BH.div BH.! A.id (BH.toValue $ show s) BH.! A.class_ "column" BH.! A.ondragover "lantis.allowDrag(event)" BH.! A.ondrop "lantis.drop(event)" $ do
     BH.h1 $ BH.toHtml s
     mapM_ card (filter (\x -> issueStatus x == s) is)
 
 card :: Issue -> BH.Markup
 card i = BH.div BH.! A.id (BH.toValue ("issue" ++ show (issueId i))) BH.! A.class_ "card" BH.! A.draggable (BH.toValue True) BH.! A.ondragstart "lantis.drag(event)" $
     BH.toHtml $ BH.html $ do
-      BH.button BH.! A.class_ "deleteButton" BH.! A.onclick  (BH.toValue $ "lantis.deleteIssue(" ++ show (issueId i) ++ ")") $ "Delete issue"
+      BH.button BH.! A.class_ "delete" BH.! A.onclick  (BH.toValue $ "lantis.deleteIssue(" ++ show (issueId i) ++ ")") $ "X"
       BH.h2 $ BH.string (T.unpack $ issueSummary i)
       BH.ul $ 
                BH.li $ BH.toMarkup $ "#" ++ show (issueId i)
